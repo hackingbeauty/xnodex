bfdff3fa217ec4d28be407de1555c450ef7c5434
Mark Muskardin <markmuskardin@mark-muskardins-macbook-pro.local>
2010-09-08 22:04:54 -0700

    starting from scratch

--- server.js
+++ server.js
@@ -1,53 +1,7 @@
-var kiwi= require('kiwi')
+var http = require('http'),
+    port = process.env.PORT || 8001
 
-kiwi.require('express');
-require('express/plugins')
-var ArticleProvider = require('./articleprovider-memory').ArticleProvider;
-
-configure(function(){
-  use(MethodOverride);
-  use(ContentLength);
-  use(Logger);
-  set('root', __dirname);
-})
-
-var articleProvider= new ArticleProvider();
-
-get('/', function(){
-  var self = this;
-  articleProvider.findAll(function(error, docs){
-    self.render('blogs_index.html.haml', {
-      locals: {
-        title: 'Blog',
-        articles: docs
-      }
-    });
-  })
-})
-
-get('/*.css', function(file){
-	this.render(file + '.css.sass', {layout: false});
-});
-
-get('/blog/new', function(){
-	this.render('blog_new.html.haml', {
-		locals: {
-			title: 'New Post'
-		}
-	})
-});
-
-post('/blog/new', function(){
-	var self = this;
-	articleProvider.save({
-		title: this.param('title'),
-		body: this.param('body')
-	}, function(error,docs){
-		self.redirect('/')
-	});
-});
-
-run();
-
-
-
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'})
+  res.end('Hello World\n')
+}).listen(parseInt(port))
